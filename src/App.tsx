e442eea6ac5ed2dcf814f9761f42108d5e023e09
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-06-18 16:28:13 -0300

    feat: adiciona rotas para associado, edição de associado e listagem de associados no App.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Associado from './pages/Associado';
+import EditarAssociado from './components/EditarAssociado';
 
 const App = () => {
   return (
@@ -10,6 +12,9 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/associado" element={<Associado />} />
+          <Route path="/editar-associado/:id" element={<EditarAssociado />} />
+          <Route path="/listar-associados" element={<Associado />} />
         </Routes>
         <Footer />
       </div>
