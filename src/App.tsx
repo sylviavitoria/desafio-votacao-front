d80bc953e5ba10cd5f26ed3ac1269e5be2556f6d
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-06-19 17:58:35 -0300

    feat: adicionar rotas para gestão de pautas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Associado from './pages/Associado';
 import EditarAssociado from './components/EditarAssociado';
+import Pauta from './pages/Pauta';
+import EditarPauta from './components/EditarPauta';
 
 const App = () => {
   return (
@@ -15,6 +17,9 @@ const App = () => {
           <Route path="/associado" element={<Associado />} />
           <Route path="/editar-associado/:id" element={<EditarAssociado />} />
           <Route path="/listar-associados" element={<Associado />} />
+          <Route path="/pauta" element={<Pauta />} />
+          <Route path="/editar-pauta/:id" element={<EditarPauta />} />
+          <Route path="/listar-pautas" element={<Pauta />} />
         </Routes>
         <Footer />
       </div>
