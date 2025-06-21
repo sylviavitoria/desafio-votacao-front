53078dde40d319af3af62930a62b00f230bc234c
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-06-21 17:06:17 -0300

    feat: criar página de voto

---
+++ src/pages/Voto.tsx
@@ -0,0 +1,22 @@
+import Banner from '../components/Banner';
+import BuscarVoto from '../components/BuscarVoto';
+import BuscarResultado from '../components/BuscarResultado';
+
+const Voto = () => {
+  return (
+    <>
+      <Banner
+        titulo="Consultas de Votação"
+        descricao="Visualize detalhes sobre votos individuais e resultados de pautas."
+      />
+      <div className="content">
+        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '20px' }}>
+          <BuscarVoto />
+          <BuscarResultado />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Voto;
